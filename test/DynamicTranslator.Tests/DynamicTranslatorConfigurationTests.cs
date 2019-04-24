93b104f6f465cdd4b3663faae8d2cf1035f622e8
Oguzhan Soykan <o.soykan@evision-software.com>
2019-04-24 10:54:29 +0200

    wip

---
+++ test/DynamicTranslator.Tests/DynamicTranslatorConfigurationTests.cs
@@ -0,0 +1,14 @@
+using DynamicTranslator.Configuration;
+using Xunit;
+
+namespace DynamicTranslator.Tests
+{
+    public class DynamicTranslatorConfigurationTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var config = new DynamicTranslatorConfiguration();
+        }
+    }
+}
